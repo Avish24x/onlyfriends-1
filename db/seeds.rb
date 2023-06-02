6f8285e590b1a0b545598b7e33d311c93309b93d
lea saulnier <leasaulnier@orange.fr>
2023-06-02 17:08:15 +0400

    seed 30 personnes

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,10 +30,21 @@ puts "created sport event party entertainment"
 images = [
   "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685631974/development/1cj27m66hu18nxlrnh0u0mllde3e.jpg",
   "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685629815/development/bvbqi8vihma5e8xdiyk7rccayodm.png",
-  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685626856/development/fae4o4lmepai17v1c4p0al5attig.png"
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685626856/development/fae4o4lmepai17v1c4p0al5attig.png",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709559/clay-banks-GX8KBbVmC6c-unsplash_i3x1ix.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709559/michael-dam-mEZ3PoFGs_k-unsplash_x8tzt6.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709269/d222f555b4b52852aa154cbde8d10d5f_caxeux.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709268/d8d6410ae0c317178e1318f4ca97ea21_yw9spx.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709268/cdbd10b2cdf094e27613944f8819b19f_q1qaxc.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709268/ca1d3c06d5e8554a992201ea492dfe12_vjveut.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709267/81159f3a28fd2485583af22bb9ccbaa3_uu2pon.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709268/a4c4c4b69fefdde32120eeb8f8e4a868_kmaatt.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709267/1956fdd05b46365e97637c6e365ed1e8_mqz5i8.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709268/c69866f67f2a18b4174c0234cea6091e_nxqis0.jpg",
+  "https://res.cloudinary.com/dyzvwwvns/image/upload/v1685709266/18aa589555075bb52b5cf7bdffcb8303_urh45w.jpg"
 ]
 
-2.times do |index|
+30.times do |index|
   user_seed = User.create(
     email: Faker::Internet.email,
     password: "FF1234"
